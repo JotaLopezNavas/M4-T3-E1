4b5bdeb7a8c7a3920bb825d20d7691712d31ff17
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-08 10:10:50 -0300

    colores de botones rrss

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,12 +1,12 @@
 <template>
-  <div>
+  <div id="social">
     <p class="card-text">Encuentranos en nuestras redes sociales.</p>
-    <a href="https://web.facebook.com/NorthSideYouthSoccerLeague/?_rdc=1&_rdr"><b-icon icon="facebook" font-scale="4"></b-icon></a>&nbsp;
-    <a href="https://www.instagram.com/northsidesoccerindy/"><b-icon icon="instagram" font-scale="4"></b-icon></a>&nbsp;
+    <a href="https://web.facebook.com/NorthSideYouthSoccerLeague/?_rdc=1&_rdr"><b-icon class="rrss" icon="facebook" font-scale="4"  style="color:#3B5998"></b-icon></a>&nbsp;
+    <a href="https://www.instagram.com/northsidesoccerindy/"><b-icon class="rrss" icon="instagram" font-scale="4" style="color:#8134AF;"></b-icon></a>&nbsp;
     <a
       href="https://www.youtube.com/watch?app=desktop&v=dQw4w9WgXcQ&list=PLJmvyRGClNKiEWKe-lJdfNxImgefHCfQJ&index=2"
-      ><b-icon icon="youtube" font-scale="4"></b-icon></a
+      ><b-icon class="rrss" icon="youtube" font-scale="4" style="color:red"></b-icon></a
     >&nbsp;
-    <a href="https://twitter.com/northside_fc"><b-icon icon="twitter" font-scale="4"></b-icon></a>&nbsp;
+    <a href="https://twitter.com/northside_fc"><b-icon class="rrss" icon="twitter" font-scale="4"></b-icon></a>&nbsp;
   </div>
 </template>
