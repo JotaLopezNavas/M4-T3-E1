bbbc1d53d0dc80eeef83d262a1d9fb686b4f9c9a
Ignacio Gamboa <ign.gga@outlook.cl>
2021-01-08 09:21:50 -0300

    se linkean redes sociales

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <p class="card-text">Encuentranos en nuestras redes sociales.</p>
-    <a href="#"><b-icon icon="facebook" font-scale="4"></b-icon></a>&nbsp;
-    <a href="#"><b-icon icon="instagram" font-scale="4"></b-icon></a>&nbsp;
+    <a href="https://web.facebook.com/NorthSideYouthSoccerLeague/?_rdc=1&_rdr"><b-icon icon="facebook" font-scale="4"></b-icon></a>&nbsp;
+    <a href="https://www.instagram.com/northsidesoccerindy/"><b-icon icon="instagram" font-scale="4"></b-icon></a>&nbsp;
     <a
       href="https://www.youtube.com/watch?app=desktop&v=dQw4w9WgXcQ&list=PLJmvyRGClNKiEWKe-lJdfNxImgefHCfQJ&index=2"
       ><b-icon icon="youtube" font-scale="4"></b-icon></a
     >&nbsp;
-    <a href="#"><b-icon icon="twitter" font-scale="4"></b-icon></a>&nbsp;
+    <a href="https://twitter.com/northside_fc"><b-icon icon="twitter" font-scale="4"></b-icon></a>&nbsp;
   </div>
 </template>
