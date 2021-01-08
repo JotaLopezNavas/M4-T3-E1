2eadfa281dae2fe6fa84075715aaad514b368af0
JotaLopezNavas <urumaco@gmail.com>
2021-01-08 13:32:56 -0300

    rules comp, vista y estilo

--- src/components/TablaSep.vue
+++ src/components/TablaSep.vue
@@ -1,66 +1,61 @@
     <template>
     <div id="tabla">
-        <table class="table table-dark table-striped">
-        <thead>
-          <tr>
-            <th scope="col">SEPTEMBER</th>
-            <th scope="col">Teams</th>
-            <th scope="col">Location</th>
-            <th scope="col">Times</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row" rowspan="2">9/01</th>
-            <td>U1 and U4</td>
-            <td>AJ Katzenmaier</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U3 and U2</td>
-            <td>Greenbay</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">9/08</th>
-            <td>U5 and U6</td>
-            <td>Howard A Yeager</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U6 and U1</td>
-            <td>Marjorie P Hart</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">9/15</th>
-            <td>U2 and U4</td>
-            <td>North</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U3 and U5</td>
-            <td>AJ Katzenmaier</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row" rowspan="2">9/22</th>
-            <td>U1 and U3</td>
-            <td>South</td>
-            <td>9:30 a.m.</td>
-          </tr>
-          <tr>
-            <td>U2 and U6</td>
-            <td>Howard A Yeager</td>
-            <td>1:00 p.m.</td>
-          </tr>
-          <tr>
-            <th scope="row">9/29</th>
-            <td>U4 and U5</td>
-            <td>Greenbay</td>
-            <td>9:30 a.m.</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+               <table class="table table-bordered">
+                    <tr>
+                      <th colspan="4" id="sept">SEPTEMBER</th>
+                    <tr>
+                    <th>Date</th>
+                    <th>Teams</th>
+                    <th>Location</th>
+                    <th>Times</th>
+                    </tr>
+                    <tr>
+                    <td rowspan="2">9/01</td>
+                    <td> <img src="../assets/icon/G1.png" alt="icono" class="icono">U1 and U4<img src="../assets/icon/G4.png" alt="icono" class="icono"></td>
+                    <td>AJ Katzenmaier</td>
+                    <td>9:30 a.m.</td>
+                    </tr>
+                    <tr>
+                    <td><img src="../assets/icon/G3.png" alt="icono" class="icono">U3 and U2<img src="../assets/icon/G2.png" alt="icono" class="icono"></td>
+                    <td>Greenbay</td>
+                    <td>1:00 p.m.</td>
+                    </tr>
+                    <tr>
+                    <td rowspan="2">9/08</td>
+                    <td><img src="../assets/icon/G5.png" alt="icono" class="icono">U5 and U6<img src="../assets/icon/G6.png" alt="icono" class="icono"></td>
+                    <td>Howard A Yeager</td>
+                    <td>9:30 a.m.</td>
+                    </tr>
+                    <tr>
+                    <td><img src="../assets/icon/G6.png" alt="icono" class="icono">U6 and U1<img src="../assets/icon/G1.png" alt="icono" class="icono"></td>
+                    <td>Marjorie P Hart</td>
+                    <td>1:00 p.m.</td>
+                    <tr>
+                    <td rowspan="2">9/15</td>
+                    <td><img src="../assets/icon/G2.png" alt="icono" class="icono">U2 and U4<img src="../assets/icon/G4.png" alt="icono" class="icono"></td>
+                    <td>North</td>
+                    <td>9:30 a.m.</td>
+                    </tr>
+                    <tr>
+                    <td><img src="../assets/icon/G3.png" alt="icono" class="icono">U3 and U5<img src="../assets/icon/G5.png" alt="icono" class="icono"></td>
+                    <td>AJ Katzenmaier</td>
+                    <td>1:00 p.m.</td>
+                    <tr>
+                    <td rowspan="2">9/22</td>
+                    <td><img src="../assets/icon/G1.png" alt="icono" class="icono">U1 and U3<img src="../assets/icon/G3.png" alt="icono" class="icono"></td>
+                    <td>South</td>
+                    <td>9:30 a.m.</td>
+                    </tr>
+                    <tr>
+                    <td><img src="../assets/icon/G2.png" alt="icono" class="icono">U2 and U6<img src="../assets/icon/G6.png" alt="icono" class="icono"></td>
+                    <td>Howard A Yeager</td>
+                    <td>1:00 p.m.</td>
+                    <tr>
+                    <td>9/29</td>
+                    <td><img src="../assets/icon/G4.png" alt="icono" class="icono">U4 and U5<img src="../assets/icon/G5.png" alt="icono" class="icono"></td>
+                    <td>Greenbay</td>
+                    <td>9:30 a.m.</td>
+                    </tr>
+              </table>
+      </div>
 </template>
